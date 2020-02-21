917fd01500476fde004b86f2a6b07df5b407ae6b
Nicolas Donoso <nicodonosocastex@hotmail.com>
2020-02-21 09:22:55 -0300

    done from yesterday

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,33 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
+  end
+
+  # All the create/new thing
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.delete
+    redirect_to cocktails_path()
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'cocktails', to: 'cocktails#index'
+  get 'cocktails/new', to: 'cocktails#new'
+  get 'cocktails/:id', to: 'cocktails#show', as: :cocktail
+  post 'cocktails', to: 'cocktails#create'
+  delete 'cocktails/:id', to: 'cocktails#destroy'
+  get 'cocktails/:cocktail_id/doses/new', to: 'doses#new'
+  post 'cocktails/:cocktail_id/doses', to: 'doses#create', as: :cocktail_doses
+  delete '/doses/:id', to: 'doses#destroy', as: :dose
+  # resources :cocktails, except: [:edit, :update] do
+  #   resources :doses, only: [:new, :create]
+  # end
+
+  # resources :doses, only: [:destroy]
 end
